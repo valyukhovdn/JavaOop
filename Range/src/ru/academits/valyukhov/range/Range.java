208c5ff4eb5a1791293ea6161ba527891590f2d6
valyukhov_dn <sibtorg2002@ngs.ru>
2023-07-20 12:52:57 +0700

    Second commit of task "Range*".

--- Range/src/ru/academits/valyukhov/range/Range.java
+++ Range/src/ru/academits/valyukhov/range/Range.java
@@ -32,4 +32,95 @@ public class Range {
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
+
+    @Override
+    public String toString() {
+        return "(" + from + "; " + to + ")";
+    }
+
+    public static void printResult(Range[] range) {
+        System.out.print("[");
+
+        int i = 0;
+
+        for (Range e : range) {
+            if (i == 1) {
+                System.out.print(", ");
+            }
+
+            System.out.print(e);
+
+            ++i;
+
+        }
+
+        System.out.println("]");
+    }
+
+
+    //  Определение интервала-пересечения двух интервалов.
+    public Range[] getIntersection(Range range) {
+        if (this.to <= range.from || range.to <= this.from) {  //  Проверка на отсутствие пересечений.
+            return new Range[]{null};
+        }
+
+        if (range.from <= this.from && this.to <= range.to) {  //  Проверяем, входит ли 1-й интервал во 2-й.
+            return new Range[]{new Range(this.from, this.to)};             // Новый объект с диапазоном range1
+        }
+
+        if (this.from <= range.from && range.to <= this.to) {  //  Проверяем, входит ли 2-й интервал в 1-й.
+            return new Range[]{new Range(range.from, range.to)};             // Новый объект с диапазоном range2
+        }
+
+        if (this.from <= range.from) {  // Проверяем, какой интервал начинается раньше.
+            return new Range[]{new Range(range.from, this.to)};
+        }
+
+        return new Range[]{new Range(this.from, range.to)};
+    }
+
+    //  Определение объединения двух интервалов.
+    public Range[] getUnion(Range range) {
+        if (this.to < range.from || range.to < this.from) {  //  Проверка на отсутствие пересечений.
+            return new Range[]{this, range};
+        }
+
+        if (range.from <= this.from && this.to <= range.to) {  //  Проверяем, входит ли 1-й интервал во 2-й.
+            return new Range[]{range};
+        }
+
+        if (this.from <= range.from && range.to <= this.to) {  //  Проверяем, входит ли 2-й интервал в 1-й.
+            return new Range[]{this};
+        }
+
+        if (this.from <= range.from) {  // Проверяем, какой интервал начинается раньше.
+            return new Range[]{new Range(this.from, range.to)};
+        }
+
+        return new Range[]{new Range(range.from, this.to)};
+    }
+
+    //  Определение разности между первым и вторым интервалом.
+    public Range[] getDifference(Range range) {
+        if (this.to <= range.from || range.to <= this.from) {  //  Проверка на отсутствие пересечений.
+            return new Range[]{this};
+        }
+
+        if (range.from <= this.from && this.to <= range.to) {  //  Проверяем, входит ли 1-й интервал во 2-й.
+            return new Range[]{};
+        }
+
+        if (this.from <= range.from && range.to <= this.to) {  //  Проверяем, входит ли 2-й интервал в 1-й.
+            return new Range[]{
+                    new Range(this.from, range.from),
+                    new Range(range.to, this.to)
+            };
+        }
+
+        if (this.from <= range.from) {  // Проверяем, какой интервал начинается раньше.
+            return new Range[]{new Range(this.from, range.from)};
+        }
+
+        return new Range[]{new Range(range.to, this.to)};
+    }
 }
--- Range/src/ru/academits/valyukhov/range_main/RangeMain.java
+++ Range/src/ru/academits/valyukhov/range_main/RangeMain.java
@@ -8,39 +8,96 @@ public class RangeMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите начальное (наименьшее) вещественное число диапазона: ");
+        System.out.print("Введите начальное (наименьшее) вещественное число ПЕРВОГО диапазона: ");
         double from = scanner.nextDouble();
 
-        System.out.print("Введите конечное (наибольшее) вещественное число диапазона: ");
+        System.out.print("Введите конечное (наибольшее) вещественное число ПЕРВОГО диапазона: ");
         double to = scanner.nextDouble();
 
         if (from > to) {
             System.out.println("Ошибка! Конечное число диапазона должно быть больше начального.");
         } else {
-            Range range = new Range(from, to);
-            System.out.println("Длина указанного диапазона: " + range.getLength());
+            Range range1 = new Range(from, to);
+            System.out.println("ПЕРВЫЙ диапазон: (" + range1.getFrom() + "; " + range1.getTo() + ")");
+            System.out.println("Длина ПЕРВОГО диапазона: " + range1.getLength());
 
-            System.out.printf("Введите вещественное число, входящее в указанный диапазон (от %f до %f): ", from, to);
+            System.out.printf("%nВведите вещественное число, входящее в указанный диапазон (от %f до %f): ", from, to);
             double userNumber = scanner.nextDouble();
 
-            if (range.isInside(userNumber)) {
-                System.out.printf("Правильно, введённое Вами число %f принадлежит указанну диапазону.", userNumber);
+            if (range1.isInside(userNumber)) {
+                System.out.printf("Правильно, введённое Вами число %f принадлежит указанну диапазону.%n", userNumber);
             } else {
-                System.out.printf("Введённое Вами число %f находится вне указанного диапазона.%n", userNumber);
+                System.out.printf("Введённое Вами число %f находится вне указанного диапазона.%n%n", userNumber);
                 System.out.printf("Теперь измените диапазон так, чтобы число %f входило в него.%n", userNumber);
 
-                System.out.print("Введите новое начальное вещественное число диапазона: ");
-                range.setFrom(scanner.nextDouble());
+                System.out.print("Введите новое начальное вещественное число ПЕРВОГО диапазона: ");
+                range1.setFrom(scanner.nextDouble());
 
-                System.out.print("Введите новое конечное вещественное число диапазона: ");
-                range.setTo(scanner.nextDouble());
+                System.out.print("Введите новое конечное вещественное число ПЕРВОГО диапазона: ");
+                range1.setTo(scanner.nextDouble());
 
-                if (range.isInside(userNumber)) {
-                    System.out.printf("Теперь ранее введённое Вами число %f принадлежит новому диапазону.", userNumber);
+                if (range1.getFrom() > range1.getTo()) {
+                    System.out.println("ОШИБКА! Конечное число диапазона должно быть больше начального.");
+                    System.exit(0);
                 } else {
-                    System.out.printf("Введённое Вами ранее число %f находится вне нового диапазона.%n", userNumber);
-                    System.out.printf("Обратите внимание, Вы указали начало диапазона %f, конец диапазона %f, " +
-                            "a ведённое Вами число имеет значение %f.", range.getFrom(), range.getTo(), userNumber);
+                    if (range1.isInside(userNumber)) {
+                        System.out.printf("Теперь ранее введённое Вами число %f принадлежит обновлённому ПЕРВОМУ диапазону.%n", userNumber);
+                    } else {
+                        System.out.printf("Введённое Вами ранее число %f находится вне обновлённого ПЕРВОГО диапазона.%n", userNumber);
+                        System.out.printf("Обратите внимание, Вы указали начало диапазона %f, конец диапазона %f, " +
+                                "a ведённое Вами число имеет значение %f.%n", range1.getFrom(), range1.getTo(), userNumber);
+                    }
+                }
+            }
+
+            System.out.println();
+
+            System.out.print("Введите начальное (наименьшее) вещественное число ВТОРОГО диапазона: ");
+            from = scanner.nextDouble();
+
+            System.out.print("Введите конечное (наибольшее) вещественное число ВТОРОГО диапазона: ");
+            to = scanner.nextDouble();
+
+            if (from > to) {
+                System.out.println("ОШИБКА! Конечное число диапазона должно быть больше начального.");
+                System.exit(0);
+            } else {
+                Range range2 = new Range(from, to);
+                System.out.println("ВТОРОЙ диапазон: (" + range2.getFrom() + "; " + range2.getTo() + ")");
+                System.out.println();
+
+                // Пересечение двух интервалов.
+                Range[] intersection = range1.getIntersection(range2);
+
+                if (intersection[0] == null) {
+                    System.out.println("Указанные диапазоны не пересекаются!");
+                } else {
+                    System.out.print("Интервал-пересечения ПЕРВОГО и ВТОРОГО диапазонов: ");
+                    Range.printResult(intersection);
+                }
+
+                //  Объединение двух интервалов.
+                Range[] union = range1.getUnion(range2);
+
+                if (union.length == 1) {
+                    System.out.print("Объединением ПЕРВОГО и ВТОРОГО диапазонов является интервал: ");
+                    Range.printResult(union);
+                } else {
+                    System.out.print("Объединением ПЕРВОГО и ВТОРОГО диапазонов являются 2 интервала: ");
+                    Range.printResult(union);
+                }
+
+                //  Разность между первым и вторым интервалом.
+                Range[] difference = range1.getDifference(range2);
+
+                if (difference.length == 0) {
+                    System.out.println("Разность ПЕРВОГО и ВТОРОГО диапазонов равна: 0");
+                } else if (difference.length == 1) {
+                    System.out.print("Разностью ПЕРВОГО и ВТОРОГО диапазонов является интервал: ");
+                    Range.printResult(difference);
+                } else {
+                    System.out.print("Разностью ПЕРВОГО и ВТОРОГО диапазонов являются 2 интервала: ");
+                    Range.printResult(difference);
                 }
             }
         }
