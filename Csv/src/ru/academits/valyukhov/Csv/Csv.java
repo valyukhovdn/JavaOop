77c89acb32243134cc38eb10d12e2a493fe7beb4
valyukhov_dn <sibtorg2002@yandex.ru>
2024-08-01 17:25:45 +0700

    Корректировка кода задачи CSV с учётом замечаний в письме от 29.07.2024.

--- Csv/src/ru/academits/valyukhov/Csv/Csv.java
+++ Csv/src/ru/academits/valyukhov/Csv/Csv.java
@@ -2,7 +2,6 @@ package ru.academits.valyukhov.Csv;
 
 import java.io.*;
 import java.nio.charset.Charset;
-import java.util.NoSuchElementException;
 
 public class Csv {
     private static void printCharacter(PrintWriter writer, char currentChar) {
@@ -17,190 +16,173 @@ public class Csv {
         }
     }
 
-    private static int convertCsvToHtml(BufferedReader reader, PrintWriter writer) throws IOException {
+    private static boolean isConvertedCsvToHtml(BufferedReader reader, PrintWriter writer) throws IOException {
         writer.println("""
-                <!DOCTYPE html>
-                <html lang="ru">
-                
-                <head>
-                    <meta charset="UTF-8"/>
-                    <title>HTML-таблица из CSV-файла</title>
-                    <style>
-                        html {
-                            font-family: calibri;
-                        }
-                
-                        table {
-                            border-collapse: collapse;
-                            font-size: 0.9rem;
-                        }
-                
-                        td {
-                            padding: 3px;
-                            vertical-align: top;
-                        }
-                    </style>
-                </head>
-               
-                <body>
-                    <table border="1">
-                
-                        <colgroup>
-                            <col style="width: 450px" />
-                        </colgroup>
-                """);
+                 <!DOCTYPE html>
+                 <html lang="ru">
+                                \s
+                 <head>
+                     <meta charset="UTF-8"/>
+                     <title>HTML-таблица из CSV-файла</title>
+                     <style>
+                         html {
+                             font-family: calibri;
+                         }
+                                \s
+                         table {
+                             border-collapse: collapse;
+                             font-size: 0.9rem;
+                         }
+                                \s
+                         td {
+                             padding: 3px;
+                             vertical-align: top;
+                         }
+                     </style>
+                 </head>
+                                \s
+                 <body>
+                     <table border="1">
+                         <colgroup>
+                             <col style="width: 450px" />
+                         </colgroup>
+                \s""");
 
-        if (!reader.ready()) {
-            writer.println("""
-                        </table>
-                    </body>
-                    </html>
-                    """);
-
-            return 0;
-        }
-
-        String currentLine = reader.readLine();
-        int currentCharIndex = 0;
-        char currentChar = currentLine.charAt(currentCharIndex);
-
-        boolean isTableEnd = false;
-
-        while (!isTableEnd) {
-            writer.println("        <tr>");
-
-            boolean isRowEnd = false;
-
-            while (!isRowEnd) {
-                writer.print("            <td>");
-
-                boolean isDetailEnd = false;
-
-                if (currentChar == '"') {   // если в ячейке есть запятая, перевод строки или двойная кавычка
-                    while (!isDetailEnd) {
-                        if (currentCharIndex >= currentLine.length() - 1) {
-                            writer.println(currentChar);
-                            writer.print("                <br />");
-                            currentLine = reader.readLine();
-                            currentCharIndex = -1;
-                            continue;
-                        }
-
-                        ++currentCharIndex;
-                        currentChar = currentLine.charAt(currentCharIndex);
-
-                        if (currentChar == '"') {
-                            if (currentCharIndex >= currentLine.length() - 1) {
-                                isDetailEnd = true;
-                                isRowEnd = true;
-                                continue;
-                            }
-
-                            ++currentCharIndex;
-                            currentChar = currentLine.charAt(currentCharIndex);
-
-                            if (currentChar == '"') {
-                                writer.print('"');
-                                continue;
-                            }
-
-                            if (currentChar == ',') {
-                                isDetailEnd = true;
-
-                                if (currentCharIndex >= currentLine.length() - 1) {
-                                    writer.println("</td>");
-                                    writer.print("            <td>");
-                                    isRowEnd = true;
-                                } else {
-                                    ++currentCharIndex;
-                                    currentChar = currentLine.charAt(currentCharIndex);
-                                }
-
-                                continue;
-                            }
-                        }
-
-                        printCharacter(writer, currentChar);
-                    }
-                } else {   // если в ячейке НЕТ запятой, перевода строки или двойной кавычки
-                    while (!isDetailEnd) {
-                        if (currentChar == '"') {
-                            throw new NoSuchElementException();
-                        }
-
-                        if (currentChar == ',') {
-                            isDetailEnd = true;
-
-                            if (currentCharIndex >= currentLine.length() - 1) {
-                                writer.println("</td>");
-                                writer.print("            <td>");
-                                isRowEnd = true;
-                            } else {
-                                ++currentCharIndex;
-                                currentChar = currentLine.charAt(currentCharIndex);
-                            }
-
-                            continue;
-                        }
-
-                        if (currentCharIndex >= currentLine.length() - 1) {
-                            writer.print(currentChar);
-                            isDetailEnd = true;
-                            isRowEnd = true;
-                            continue;
-                        }
-
-                        printCharacter(writer, currentChar);
-
-                        ++currentCharIndex;
-                        currentChar = currentLine.charAt(currentCharIndex);
-                    }
-                }
-
-                writer.println("</td>");
-            }
-
-            writer.println("        </tr>");
-
-            if (!reader.ready()) {
-                isTableEnd = true;
-                continue;
-            }
-
-            currentLine = reader.readLine();
-            currentCharIndex = 0;
-            currentChar = currentLine.charAt(currentCharIndex);
-        }
-
-        writer.println("""
+        String tableEnd = """
                     </table>
                 </body>
                 </html>
-                """);
+                """;
 
-        return 1;
+        String currentLine = reader.readLine();
+
+        if (currentLine == null) {
+            writer.println(tableEnd);
+
+            return false;
+        }
+
+        char currentChar;
+
+        boolean isLineFeedPossibleInDetail = false;
+
+        while (currentLine != null) {
+            if (!isLineFeedPossibleInDetail) {
+                writer.println("        <tr>");
+                writer.print("            <td>");
+            }
+
+            for (int currentCharIndex = 0; currentCharIndex <= currentLine.length() - 1; ++currentCharIndex) {
+                currentChar = currentLine.charAt(currentCharIndex);
+
+                switch (currentChar) {
+                    case '"':
+                        if (currentCharIndex >= currentLine.length() - 1) {
+                            writer.println("</td>");
+                            writer.println("        </tr>");
+                            isLineFeedPossibleInDetail = false;
+                            break;
+                        }
+
+                        isLineFeedPossibleInDetail = true;
+
+                        ++currentCharIndex;
+                        currentChar = currentLine.charAt(currentCharIndex);
+
+                        switch (currentChar) {
+                            case '"':
+                                ++currentCharIndex;
+                                currentChar = currentLine.charAt(currentCharIndex);
+
+                                switch (currentChar) {
+                                    case '"':
+                                        writer.print("\"");
+
+                                        if (currentCharIndex >= currentLine.length() - 1) {
+                                            writer.print("</td>");
+                                            writer.println("        </tr>");
+                                        }
+
+                                        break;
+                                    case ',':
+                                        writer.print("\",");
+                                        break;
+                                    default:
+                                        writer.print('"');
+                                        printCharacter(writer, currentChar);
+                                }
+
+                                break;
+                            case ',':
+                                writer.println("</td>");
+
+                                isLineFeedPossibleInDetail = false;
+
+                                if (currentCharIndex >= currentLine.length() - 1) {
+                                    writer.println("            <td></td>");
+                                    writer.println("        </tr>");
+                                } else {
+                                    writer.print("            <td>");
+                                }
+
+                                break;
+                            default:
+                                printCharacter(writer, currentChar);
+                        }
+
+                        break;
+                    case ',':
+                        writer.println("</td>");
+
+                        if (currentCharIndex >= currentLine.length() - 1) {
+                            writer.println("            <td></td>");
+                            writer.println("        </tr>");
+                        } else {
+                            writer.print("            <td>");
+                        }
+
+                        break;
+                    default:
+                        printCharacter(writer, currentChar);
+
+                        if (currentCharIndex >= currentLine.length() - 1) {
+                            if (isLineFeedPossibleInDetail) {
+                                writer.println();
+                                writer.print("                <br />");
+                            } else {
+                                writer.println("</td>");
+                                writer.println("        </tr>");
+                            }
+                        }
+                }
+            }
+
+            currentLine = reader.readLine();
+        }
+
+        writer.println(tableEnd);
+
+        return true;
     }
 
     public static void main(String[] args) {
-        // Для тестов пути к файлам:
-        // args[0]: C:\Users\User\IdeaProjects\JavaOop\Csv_input.csv
-        // args[1]: C:\Users\User\IdeaProjects\\JavaOop\Csv_output.html
+        if (args.length != 2) {
+            throw new IllegalArgumentException(String.format(System.lineSeparator() + "В программу должны быть переданы " +
+                    "\"2\" аргумента (пути к исходному CSV-файлу и итоговому HTML-файлу), а Вы передали \"%d\".", args.length));
+        }
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(args[0], Charset.forName("windows-1251")));
-             PrintWriter writer = new PrintWriter(args[1])) {
-            if (convertCsvToHtml(reader, writer) == 0) {
-                System.out.println("Исходный файл пуст. Сформирована пустая таблица.");
-            } else {
+        String inputFilePath = args[0];    // Для тестов: String inputFilePath = "C:\\Users\\User\\IdeaProjects\\JavaOop\\Csv_input.csv";
+        String outputFilePath = args[1];   // Для тестов: String outputFilePath = "C:\\Users\\User\\IdeaProjects\\JavaOop\\Csv_output.html";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath, Charset.forName("windows-1251")));
+             PrintWriter writer = new PrintWriter(outputFilePath)) {
+            if (isConvertedCsvToHtml(reader, writer)) {
                 System.out.println("HTML-файл создан.");
+            } else {
+                System.out.println("Исходный файл пуст. Сформирована пустая таблица.");
             }
         } catch (IOException e) {
             System.out.println("Произошла ошибка: " + e.getMessage());
-        } catch (NoSuchElementException e) {
-            try (PrintWriter writer = new PrintWriter(args[1])) {    // Создание пустого (без html-кода) итогового html-файла.
-                System.out.println("Исходный файл не в CSV формате, или содержит ошибку.");
-            } catch (IOException ioException) {
-                System.out.println("При попытке создания пустого html-файла произошла ошибка: " + ioException.getMessage());
-            }
         }
     }
 }
